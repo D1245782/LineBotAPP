e2d033456f3e8697eb2cbc3d5b48618914d66570
程沛瑜 <145085950+D1245782@users.noreply.github.com>
2025-05-28 09:24:23 +0800

    Update common.py

--- apps/common/common.py
+++ apps/common/common.py
@@ -18,15 +18,9 @@ line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 
 # 取得專案中的圖片連結
 def localImg(path):
+    base_url = "raw.githubusercontent.com/D1245782/LineBotAPP/main/static/images/"
+    return base_url + path
     
-    # 取得機器人網址
-    bot_url = request.url_root[:-1]
-    # 取得圖片路徑
-    img_path = 'images/' + path
-    # 取得圖片網址
-    image_url = bot_url + url_for('static', filename = img_path )
-
-    return image_url
 
 # 取得專案中的 HTML 連結
 def localHtml(path):
